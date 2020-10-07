7e3015015b2c9ec01b640e7ac019e8b73f60d68f
TerranCivilian <63529094+TerranCivilian@users.noreply.github.com>
2020-10-06 20:41:24 -0400

    ch08: add new struct example

---
+++ ch08/struct_as_class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+struct Test {
+	int i;
+	void print_i() { cout << i << '\n'; }
+	Test(int n) : i{n} { }
+};
+
+int main()
+{
+
+	Test t(2);
+	t.print_i();
+	return 0;
+}
